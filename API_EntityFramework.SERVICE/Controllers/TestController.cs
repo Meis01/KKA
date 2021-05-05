c955fb04c2ecf820e76612e0e1e2c919ab35bc1a
Meis01 <meis.baas95@hotmail.com>
2021-05-06 00:36:49 +0300

    my project

---
+++ API_EntityFramework.SERVICE/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using API_EntityFramework.SERVICE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace API_EntityFramework.SERVICE.Controllers
+{
+    public class TestController : ApiController
+    {
+      private  NorthwindEntities context = new NorthwindEntities();
+       // [EnableCors(origins:"*",headers:"*",methods:"*")]
+        [EnableCors("https://localhost:44388", "*","*")]
+      public IHttpActionResult Get()
+        {
+            context.Configuration.ProxyCreationEnabled = false;
+            var catList = context.Categories.ToList();
+            return Json(catList);
+        }
+    }
+}
